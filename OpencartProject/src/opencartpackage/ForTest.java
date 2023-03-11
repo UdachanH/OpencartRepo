2f8b95e43a032bc7f0c2f3f8465e97a31e13490e
UdachanH <chauhan.dayashankar22@gmail.com>
2023-03-11 23:43:37 +0530

    Update ForTest.java

--- OpencartProject/src/opencartpackage/ForTest.java
+++ OpencartProject/src/opencartpackage/ForTest.java
@@ -5,6 +5,7 @@ public class ForTest {
 	public static void main(String[] args) {
 		System.out.println("For testing Demo");
 		System.out.println("U****H and dd****H*138_");
+		System.out.println("add colab2");
 
 	}
 
